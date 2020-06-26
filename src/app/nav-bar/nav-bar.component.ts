0c2ac4889240411209e26dde596096de7d022a1c
Ha tuan kiet <kiet.ht@itsstraining.edu.vn>
2020-06-26 11:52:45 +0700

    'loginwithGG'

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog } from '@angular/material/dialog';
 import { SignupComponent } from '../signup/signup.component';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  providers:[UserService]
+
 })
 export class NavBarComponent implements OnInit {
-
-  constructor(public dialog: MatDialog) { }
+displayName: String;
+  constructor(public dialog: MatDialog, public authSvc: UserService) { }
 
   ngOnInit(): void {
+    this.authSvc.GetCurrentUser().then( 
+      user => this.displayName = user.displayName!=null?user.displayName: user.email);
   }
+
 
   openDialog() {
     this.dialog.open(SignupComponent, {
